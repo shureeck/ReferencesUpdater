5607b4ee0bdcfd02aa379f0d5e045e9064700680
Poliakov.A <poliakov.a@dbbest.com>
2018-10-24 11:27:17 +0300

    *Related objects name

--- src/main/java/TestListString.java
+++ src/main/java/TestListString.java
@@ -14,87 +14,86 @@ public class TestListString {
     private String category;
     private String item;
 
-    public TestListString(){
-        parentObject=EMPTY;
-       childObject=EMPTY;
-       objct=EMPTY;
-       category=EMPTY;
-       item=EMPTY;
+    public TestListString() {
+        parentObject = EMPTY;
+        childObject = EMPTY;
+        objct = EMPTY;
+        category = EMPTY;
+        item = EMPTY;
     }
 
-    public TestListString(int num, String category, String parentObject, String childObject){
+    public TestListString(int num, String category, String parentObject, String childObject) {
         this.parentObject = parentObject;
         this.childObject = childObject;
-        this.category=category;
-        this.num=num;
+        this.category = category;
+        this.num = num;
     }
 
-    public int getObjectNumber(){
+    public int getObjectNumber() {
         return num;
     }//get
-    public void setObjectNumber(int n){
-        this.num=n;
+
+    public void setObjectNumber(int n) {
+        this.num = n;
     }//set
 
-    public String getРarentObject(){
+    public String getРarentObject() {
         return parentObject;
     }//get
-    public void setРarentObject(String parentObject){
-        this.parentObject=parentObject.trim();
+
+    public void setРarentObject(String parentObject) {
+        this.parentObject = parentObject.trim();
     }//ste
 
-    public String getСhildObject(){
+    public String getСhildObject() {
         return childObject;
     }//get
-    public void setСhildObject(String childObject){
-        this.childObject=childObject.trim();
+
+    public void setСhildObject(String childObject) {
+        this.childObject = childObject.trim();
     }//set
 
-    public String getСategory(){
+    public String getСategory() {
         return category;
     }//get
-    public void setСategory(String category){
-        this.category=category.trim();
+
+    public void setСategory(String category) {
+        this.category = category.trim();
     }//set
 
-    public String getTestListLine (){
+    public String getTestListLine() {
         String line;
-        if (parentObject==null) parentObject=EMPTY;
-        line = num+COMMA+category+COMMA+parentObject+COMMA+childObject+COMMA;
+        if (parentObject == null) parentObject = EMPTY;
+        line = num + COMMA + category + COMMA + parentObject + COMMA + childObject + COMMA;
         return line;
     }//get
 
-    public String getItem(){
+    public String getItem() {
         return item;
     }//get
-    public void setItem(String item){
-        this.item=item.trim();
+
+    public void setItem(String item) {
+        this.item = item.trim();
     }//set
 
-    public String getObjectName(){
-        if (parentObject.trim()==null|parentObject.equalsIgnoreCase(EMPTY))
-            objct=childObject;
+    public String getObjectName() {
+        if (parentObject.trim() == null | parentObject.equalsIgnoreCase(EMPTY))
+            objct = childObject;
 
         else
-            objct=parentObject+DOLLAR+childObject;
+            objct = parentObject + DOLLAR + childObject;
 
         return objct;
     }//get
 
-    public TestListString nodeToTestList(Node relationObject, int number ){
+    public TestListString nodeToTestList(Node relationObject, int number) {
         TestListString testList = new TestListString();
-        String objectName =((Element)relationObject).getAttribute(NAME);
+        String objectName = ((Element) relationObject).getAttribute(NAME);
 
         testList.setObjectNumber(number);
-        testList.setСategory(((Element)relationObject).getAttribute(TYPE));
-        if (objectName.contains("$")){
-            testList.setРarentObject(objectName.substring(0,objectName.indexOf("$")));
-            testList.setСhildObject(objectName.substring(objectName.indexOf("$")+1));
-        }
-        else
-        {
-            testList.setСhildObject(objectName);
-        }
+        testList.setСategory(((Element) relationObject).getAttribute(TYPE));
+        testList.setСhildObject(objectName);
+
         return testList;
     }
 
