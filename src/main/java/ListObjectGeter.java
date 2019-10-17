4d1e8c213d2d16d72afad32d358b445a2f87184a
Poliakov.A <poliakov.a@dbbest.com>
2019-10-17 12:02:59 +0300

    *%s counter

---
+++ src/main/java/ListObjectGeter.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by Poliakov.A on 10/9/2019.
+ */
+public class ListObjectGeter {
+    public static void main(String... args) {
+        Input in = new Input();
+        String sctFile = in.input("Input *.sct file path");
+        String outFile = in.input("Input output file path file path");
+        ReadFile rf = new ReadFile();
+        ArrayList<String> lines = new ArrayList(rf.getPercentSFromFile(sctFile));
+        File tl = new File(outFile);
+
+        for (String tmp : lines) {
+            int startIdx = tmp.indexOf("fullNameSource=\"")+16;
+            int endIdx = tmp.indexOf("\"", startIdx + 1);
+            String obj = tmp.substring(startIdx, endIdx);
+            startIdx = tmp.indexOf("code=\"")+6;
+            endIdx = tmp.indexOf("\"", startIdx + 1);
+            String ai = tmp.substring(startIdx, endIdx);
+            Logger.appendToTestlist(tl,ai+","+ obj);
+            Logger.setLog(obj);
+        }
+
+
+    }
+}
--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -97,4 +97,50 @@ public class ReadFile {
 
     }// fileRead
 
+    public ArrayList<String> getPercentSFromFile(String path) {
+        ArrayList<String> result = new ArrayList<>();
+        FileReader sc = null;
+        BufferedReader buffer = null;
+
+        if (path != (null)) {
+            try {
+                sc = new FileReader(path);
+                buffer = new BufferedReader(sc);
+                String line = buffer.readLine().trim();
+
+                while (line != null) {
+                    line.replaceAll(UNVISIBLE_u202A, EMPTY);
+
+                    if (line.contains("%s")) {
+                        result.add(line.trim());
+                    }
+                    Logger.setLog(LINE_READ + line);
+                    line = buffer.readLine();
+                }//while
+
+                buffer.close();
+                sc.close();
+                Logger.setLog(FILE + new File(path).getName() + READ_SUCCESSFULLY);
+            }//try
+
+            catch (FileNotFoundException e) {
+                Logger.setLog(FILE_NOT_FOUND);
+                Logger.setLog(e.getStackTrace().toString());
+                //   e.printStackTrace();
+            }//catch;
+
+            catch (IOException e) {
+                Logger.setLog(COULD_NOT_READ_FILE);
+                Logger.setLog(e.getStackTrace().toString());
+                //  e.printStackTrace();
+            }//catch;
+        }//if
+        else {
+            Logger.setLog(PATH_TO_TESTLIST_MISSING);
+        }
+
+        return result;
+
+    }// fileRead
+
 }
